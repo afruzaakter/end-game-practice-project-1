e3ba92d28b76962e175bee0c352fa797e665d5fb
PranoySarker <pranoysarker01@gmail.com>
2022-07-13 10:13:03 +0600

    footer

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Contact from './Pages/Contact/Contact';
+import Footer from './Pages/footer/Footer';
 import Navbar from './Pages/Navbar';
 
 function App() {
   return (
     <div className='mx-16'>
-     <Navbar></Navbar>
-     <Routes>
-      <Route path='/contact' element={<Contact></Contact>}></Route>
-     </Routes>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path='/contact' element={<Contact></Contact>}></Route>
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
